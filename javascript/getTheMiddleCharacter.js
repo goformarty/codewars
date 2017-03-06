7107c2be5eb83dc83cdf0dee5ccc638e4e977db9
GoForMarty <marty.stepien@gmail.com>
2017-03-06 22:26:45 +0100

    solution 2

--- javascript/getTheMiddleCharacter.js
+++ javascript/getTheMiddleCharacter.js
@@ -10,3 +10,12 @@ function getMiddle1(s)
 {
       return s.length%2 ? s[(s.length-1)/2] : s.substring(s.length/2-1,s.length/2+1);
 }
+
+// SOLUTION 2
+
+function getMiddle2(s) {
+  var middle = s.length / 2;
+  return (s.length % 2) 
+    ? s.charAt(Math.floor(middle))
+    : s.slice(middle - 1, middle + 1);
+}
