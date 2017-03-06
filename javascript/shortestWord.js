70ccc7ea2e2f3d1ef22c8ce30a0504006972c542
GoForMarty <marty.stepien@gmail.com>
2017-03-06 17:23:28 +0100

    solution 1

--- javascript/shortestWord.js
+++ javascript/shortestWord.js
@@ -1,5 +1,21 @@
 /*
-x Simple, given a string of words, return the length of the shortest word(s).
+x Simple, given a string of words, 
+return the length of the shortest word(s).
 
-String will never be empty and you do not need to account for different data types.
+String will never be empty and you do not need 
+to account for different data types.
 */
+
+
+// SOLUTION 1
+function findShort1(s){
+  var arr = s.split(" ");
+  var min = arr[0].length;
+  for (var i in arr) {
+    if (arr[i].length < min) {
+      min = arr[i].length;
+    }
+  }
+  return min;
+}
+
