437108b4651ad91fad565aa5e46f7ce74f9ef6bd
GoForMarty <marty.stepien@gmail.com>
2017-03-07 11:24:52 +0100

    Solution 2

--- javascript/grasshopperSummation.js
+++ javascript/grasshopperSummation.js
@@ -11,3 +11,15 @@ function summation1(num) {
   return num * (num + 1) / 2
 }
 
+// SOLUTION 2 === SOLUTION 1 with arrow function expression
+function summation1(num) {
+  return num * (num + 1) / 2
+}
+
+function summation2(num) {
+  var sum = 0;
+  for (var i = 0; i<=num; i++) {
+  sum += i;
+  }
+  return sum;
+};
