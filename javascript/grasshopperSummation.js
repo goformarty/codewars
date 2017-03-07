01dc74e4643ff562a2a0326a3894fd8e77a01d38
GoForMarty <marty.stepien@gmail.com>
2017-03-07 11:08:40 +0100

    solution 1

---
+++ javascript/grasshopperSummation.js
@@ -0,0 +1,13 @@
+/* 
+Summation
+
+Write a program that finds the summation of every number between 1 and num. 
+The number will always be a positive integer greater than 0.
+
+*/
+
+//SOLUTION 1 using arithmetic progression 
+function summation1(num) {
+  return num * (num + 1) / 2
+}
+
