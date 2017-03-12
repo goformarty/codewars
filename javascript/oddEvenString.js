d4c6d3ac7f5c2089ebe56bb2c6e087b8005846bd
GoForMarty <marty.stepien@gmail.com>
2017-03-12 19:09:02 +0100

    initial commit

---
+++ javascript/oddEvenString.js
@@ -0,0 +1,11 @@
+/* Description:
+
+Given a string S. 
+You have to return another string such that even-indexed 
+and odd-indexed characters of S are grouped and groups 
+are space-separated (see sample below) 
+
+input: 'CodeWars' output 'CdWr oeas'
+
+*/
+
