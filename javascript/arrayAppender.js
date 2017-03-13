328bc5b467359a5ce056017a2de2f494aaaa81d7
GoForMarty <marty.stepien@gmail.com>
2017-03-14 00:24:49 +0100

    initial commit

---
+++ javascript/arrayAppender.js
@@ -0,0 +1,10 @@
+/* Write a function called appendArrays that appends the items 
+from array 2 onto array 1, returning the newly formed array.
+
+For example if your 2 arrays were:
+var array1 = [a,b,c]
+var array2 = [1,2,3]
+
+After using your appendArrays function, the result should be [a,b,c,1,2,3]
+
+Your function should also be able to handle nested arrays.*/
