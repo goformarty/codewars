363c9f50aedc8c4f1d41c898f39beb40c6f1fff9
GoForMarty <marty.stepien@gmail.com>
2017-03-23 15:54:46 +0100

    solution 1

--- javascript/extractDomainFromURL.js
+++ javascript/extractDomainFromURL.js
@@ -8,3 +8,25 @@ domainName("https://www.cnet.com") == "cnet"
 
 */
 
+// SOLUTION 1
+// using slice()
+function domainName1(url){
+  var substring='';
+  var index;
+  
+  if (url.indexOf('www.')!==-1) {
+    substring=url.slice(url.indexOf('www.') + 4);
+  }
+  else if(url.indexOf('https')!==-1) {
+    substring=url.slice(8);
+  } 
+  else if(url.indexOf('http')!==-1) {
+    substring=url.slice(7);
+  }
+  else {
+    substring=url;
+  }
+
+  index = substring.indexOf('.');
+  return substring.slice(0, index);
+}
