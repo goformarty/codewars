fb198193483add52ac284d06558698be89aeda68
Marty Stepien <marty.stepien@gmail.com>
2018-03-05 17:57:01 +0000

    sum of pairs loop solution

---
+++ sumOfPositive.js
@@ -0,0 +1,14 @@
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: array may be empty, in this case return 0.
+
+function positiveSum(arr) {
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] > 0)
+			sum += arr[i];
+	}
+	return sum;
+}
